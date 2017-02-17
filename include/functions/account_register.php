6b246f1688b7ec5c6abfc1f3a271a1714fd67757
Nathaniel Suchy <nsuchy@users.noreply.github.com>
2017-02-17 12:32:05 -0500

    Removed SQL Injection Risks, Organized Code, Etc
    Commented code out in the bottom until I talk to the contributor to see it's purpose. I'm a little confused at what he was trying to do.

--- include/functions/account_register.php
+++ include/functions/account_register.php
@@ -1,19 +1,28 @@
 <?php
+/* 
+    icebb 0.1
+    Copyright 2017 - Nathaniel Suchy
+    License: GPL3.0
+*/
 
+// Prepare connection to the database
 include "pdo-mysqli.php";
 
+// If connecting to the database fails
 if(! $conn ) {
 die('Could not connect: ' . mysql_error());
 }
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$passwordv = $_POST['passwordv'];
-$email = $_POST['email'];
-$dobday = $_POST['dobday'];
-$dobmonth = $_POST['dobmonth'];
-$dobyear = $_POST['dobyear'];
+// Assign the post request data to variables.
+$username = mysqli_real_escape_string($_POST['username']);
+$password = mysqli_real_escape_string($_POST['password']);
+$passwordv = mysqli_real_escape_string($_POST['passwordv']);
+$email = mysqli_real_escape_string($_POST['email']);
+$dobday = mysqli_real_escape_string($_POST['dobday']);
+$dobmonth = mysqli_real_escape_string($_POST['dobmonth']);
+$dobyear = mysqli_real_escape_string($_POST['dobyear']);
 
+// Input the variables into the database
 function register($username, $password, $email, $dobday, $dobmonth, $dobyear) {
 
      $sql = 'INSERT INTO users '.
@@ -22,7 +31,8 @@ function register($username, $password, $email, $dobday, $dobmonth, $dobyear) {
   
 }
 
-if (// all info verify) {
+/* if (// all info verify) {
   register($username, $password, $email, $dobday, $dobmonth, $dobyear);
-  }
+  } */
+
 ?>
