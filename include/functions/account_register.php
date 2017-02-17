8d8de94f6bfa98a70fdbb3c6ce126f59757a743d
z3us0v3rl0rd <z3us0v3rl0rd@gmail.com>
2017-02-17 13:17:45 +0530

    Update account_register.php
    Addition of main base.

--- include/functions/account_register.php
+++ include/functions/account_register.php
@@ -1,4 +1,28 @@
 <?php
-function SaySomeThingScript() {
-echo "help me!!!";
+
+include "pdo-mysqli.php";
+
+if(! $conn ) {
+die('Could not connect: ' . mysql_error());
 }
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$passwordv = $_POST['passwordv'];
+$email = $_POST['email'];
+$dobday = $_POST['dobday'];
+$dobmonth = $_POST['dobmonth'];
+$dobyear = $_POST['dobyear'];
+
+function register($username, $password, $email, $dobday, $dobmonth, $dobyear) {
+
+     $sql = 'INSERT INTO users '.
+      '(username, password, email, dobdate, dobmonth, dobyear, join_date) '.
+      'VALUES ($username, $password, $email, $dobday, $dobmonth, $dobyear, NOW() )';
+  
+}
+
+if (// all info verify) {
+  register($username, $password, $email, $dobday, $dobmonth, $dobyear);
+  }
+?>
